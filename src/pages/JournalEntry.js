ada1a0187f601679827a1a73093d16f739cc6218
Sandy <sandyddinh@gmail.com>
2021-02-13 15:03:13 -0500

    show page works

--- src/pages/JournalEntry.js
+++ src/pages/JournalEntry.js
@@ -3,6 +3,10 @@ import moment from 'moment';
 
 export default function App(props) {
 	const [entry, setEntry] = useState([]);
+	const [feelings, setFeelings] = useState('');
+	const [care, setCare] = useState('');
+	const [manifestations, setManifestations] = useState('');
+	const [goals, setGoals] = useState('');
 
 	useEffect(() => {
 		(async () => {
@@ -10,34 +14,54 @@ export default function App(props) {
 				const response = await fetch(`/api/journal/${props.match.params.id}`);
 				const data = await response.json();
 				setEntry(data);
+				setFeelings(data.feelings);
+				setCare(data.care);
+				setManifestations(data.manifestations);
+				setGoals(data.goals);
 			} catch (error) {
 				console.error(error);
 			}
 		})();
 	}, []);
 
-	const createList = str => {
-		const splitStr = str.split(',');
-		console.log(splitStr);
-		splitStr.map(item => {
-			return <li>{item}</li>;
-		});
-	};
-
 	return (
 		<div className="show-journal-entry">
 			{entry.time ? moment(entry.time).format('MMMM Do YYYY') : ''} <br />
 			<h4>Today I am feeling..</h4>
-			<ul>{entry.feelings ? createList(entry.feelings) : ''}</ul>
+			<ul>
+				{entry.feelings !== ''
+					? feelings.split(',').map(item => {
+							return <li key={item}>{item}</li>;
+					  })
+					: ''}
+			</ul>
 			<br />
 			<h4>How I'm taking care of myself..</h4>
-			<ul>{entry.care ? createList(entry.care) : ''}</ul>
+			<ul>
+				{entry.care !== ''
+					? care.split(',').map(item => {
+							return <li key={item}>{item}</li>;
+					  })
+					: ''}
+			</ul>
 			<br />
 			<h4>What I'm manifesting.. </h4>
-			<ul>{entry.manifestations ? createList(entry.manifestations) : ''}</ul>
+			<ul>
+				{entry.manifestations !== ''
+					? manifestations.split(',').map(item => {
+							return <li key={item}>{item}</li>;
+					  })
+					: ''}
+			</ul>
 			<br />
 			<h4>My future goals..</h4>
-			<ul>{entry.goals ? createList(entry.goals) : ''}</ul>
+			<ul>
+				{entry.goals !== ''
+					? goals.split(',').map(item => {
+							return <li key={item}>{item}</li>;
+					  })
+					: ''}
+			</ul>
 		</div>
 	);
 }
