039b4c9bfae10d0671a95b33d9f6f7780a569765
Sandy <sandyddinh@gmail.com>
2021-02-17 22:25:46 -0500

    styled the show page

--- src/pages/JournalEntry.js
+++ src/pages/JournalEntry.js
@@ -29,48 +29,74 @@ export default function App(props) {
 	}, []);
 
 	return (
-		<div className="show-journal-entry">
-			{entry.time ? moment(entry.time).format('MMMM Do YYYY') : ''} <br />
-			<h2>{Object.keys(quote) ? quote.text : ''}</h2>
-			<h3>{Object.keys(quote) ? quote.author : ''}</h3>
-			<Link to={`/${entry._id}/edit`}>
-				<button className="update-button">Update Journal Entry</button>
-			</Link>
-			<h4>Today I am feeling..</h4>
-			<ul>
-				{feelings?.includes(',')
-					? feelings.split(',').map(item => {
-							return <li key={item}>{item}</li>;
-					  })
-					: feelings}
-			</ul>
-			<br />
-			<h4>How I'm taking care of myself..</h4>
-			<ul>
-				{care?.includes(',')
-					? care.split(',').map(item => {
-							return <li key={item}>{item}</li>;
-					  })
-					: care}
-			</ul>
-			<br />
-			<h4>What I'm manifesting.. </h4>
-			<ul>
-				{manifestations?.includes(',')
-					? manifestations.split(',').map(item => {
-							return <li key={item}>{item}</li>;
-					  })
-					: manifestations}
-			</ul>
-			<br />
-			<h4>My future goals..</h4>
-			<ul>
-				{goals?.includes(',')
-					? goals.split(',').map(item => {
-							return <li key={item}>{item}</li>;
-					  })
-					: goals}
-			</ul>
+		<div className="JournalEntryPage">
+			<div className="top date">
+				{entry.time ? moment(entry.time).format('MMMM Do YYYY') : ''} <br />
+			</div>
+
+			<div className="top">
+				<div className="quote">
+					{Object.keys(quote) ? quote.text : ''} <br />-{' '}
+					{Object.keys(quote) ? quote.author : 'unknown'}
+				</div>
+			</div>
+
+			{/* <h2>{Object.keys(quote) ? quote.text : ''}</h2>
+			<h3>{Object.keys(quote) ? quote.author : ''}</h3> */}
+			<div className="middle">
+				<div className="col1">
+					<p className="input-label">My Daily Diary</p>
+					<div className="input-area feelings">
+						<ul>
+							{feelings?.includes(',')
+								? feelings.split(',').map(item => {
+										return <li key={item}>{item}</li>;
+								  })
+								: feelings}
+						</ul>
+					</div>
+				</div>
+
+				<div className="col2">
+					<p className="input-label">How I'm taking care of myself today</p>
+					<div className="input-area">
+						<ul>
+							{care?.includes(',')
+								? care.split(',').map(item => {
+										return <li key={item}>{item}</li>;
+								  })
+								: care}
+						</ul>
+					</div>
+					<br />
+					<p className="input-label">My goals for today</p>
+					<div className="input-area">
+						<ul>
+							{goals?.includes(',')
+								? goals.split(',').map(item => {
+										return <li key={item}>{item}</li>;
+								  })
+								: goals}
+						</ul>
+					</div>
+					<br />
+					<p className="input-label">What I'm manifesting</p>
+					<div className="input-area">
+						<ul>
+							{manifestations?.includes(',')
+								? manifestations.split(',').map(item => {
+										return <li key={item}>{item}</li>;
+								  })
+								: manifestations}
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div className="bottom">
+				<Link to={`/${entry._id}/edit`}>
+					<button className="update-button">Update Journal Entry</button>
+				</Link>
+			</div>
 		</div>
 	);
 }
