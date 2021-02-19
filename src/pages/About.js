ce3a805d4b560a32357efb5e33bbd61e2b76b5d4
Sandy <sandyddinh@gmail.com>
2021-02-18 23:01:34 -0500

    Added dummy text to About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,39 @@
 import React, { useState } from 'react';
 
 export default function About(props) {
-	return <div className="AboutPage">This is the {props.page} page</div>;
+	return (
+		<div className="AboutPage">
+			<section>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris sit amet massa vitae tortor. Nec ultrices dui sapien eget mi proin sed libero. Bibendum ut tristique et egestas quis ipsum suspendisse. Pellentesque habitant morbi tristique senectus et netus. Morbi enim nunc faucibus a pellentesque. Amet commodo nulla facilisi nullam vehicula. A lacus vestibulum sed arcu. Fusce id velit ut tortor pretium.
+				</p>
+				<img src=""/>
+			</section>	
+			<section>
+				<img src=""/>
+				<p>
+					Felis bibendum ut tristique et egestas quis ipsum. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper. Habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim. Nibh nisl condimentum id venenatis a condimentum.
+				</p>
+			</section>
+			<section>
+				<p>
+					Aenean sed adipiscing diam donec. Facilisis sed odio morbi quis commodo odio aenean sed adipiscing. Fermentum dui faucibus in ornare quam viverra orci sagittis eu. A scelerisque purus semper eget duis. Et malesuada fames ac turpis egestas.
+				</p>
+				<img src=""/>
+			</section>
+			<section>
+				<p>
+					Pretium fusce id velit ut tortor pretium viverra suspendisse. At volutpat diam ut venenatis tellus in metus. Mi sit amet mauris commodo quis imperdiet massa tincidunt. Purus gravida quis blandit turpis.
+				</p>
+				<img src=""/>
+			</section>
+			<section>
+				<img src=""/>
+				<p>
+					Euismod in pellentesque massa placerat duis ultricies lacus sed turpis. Nibh tellus molestie nunc non. Faucibus vitae aliquet nec ullamcorper sit amet risus. Eu non diam phasellus vestibulum lorem. Facilisis sed odio morbi quis commodo odio aenean sed. Id velit ut tortor pretium viverra suspendisse. Sapien nec sagittis aliquam malesuada.
+				</p>
+			</section>	
+		</div>
+		
+	);
 }
